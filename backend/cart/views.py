6b4d41bd61791ed0c36ab31d61cea0dd0069b52c
Shrishti <shrishtiagarwal205@gmail.com>
2026-07-28 12:16:48 +0530

    Enhanced cart API and completed JWT authentication integration

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics, permissions
 from .models import Cart
 from .serializers import CartSerializer
 from products.models import Product
+from rest_framework import status
+from rest_framework.response import Response
 
 
 class CartListView(generics.ListAPIView):
@@ -16,12 +18,25 @@ class CartAddView(generics.CreateAPIView):
     serializer_class = CartSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def perform_create(self, serializer):
+    def create(self, request, *args, **kwargs):
+
         product = Product.objects.get(pk=self.kwargs["product_id"])
-        serializer.save(
-            user=self.request.user,
-            product=product
+
+        cart_item, created = Cart.objects.get_or_create(
+            user=request.user,
+            product=product,
+            defaults={
+                "quantity": int(request.data.get("quantity", 1))
+            },
         )
+
+        if not created:
+            cart_item.quantity += int(request.data.get("quantity", 1))
+            cart_item.save()
+
+        serializer = CartSerializer(cart_item)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class CartUpdateView(generics.RetrieveUpdateAPIView):
