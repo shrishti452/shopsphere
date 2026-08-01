bebdbf1cc077a317e0c8af52ce094caeb8d51367
Shrishti <shrishtiagarwal205@gmail.com>
2026-08-01 14:11:57 +0530

    Connect frontend to deployed backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://shopsphere-backend-y3jo.onrender.com/api/",,
+    baseURL: "https://shopsphere-backend-y3jo.onrender.com/api/",
     headers: {
         "Content-Type": "application/json",
     },
